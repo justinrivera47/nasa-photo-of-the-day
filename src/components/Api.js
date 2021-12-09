5812a90e9b92ffe686d5f55d5f030d690d721fdc
justinrivera47 <justinrivera47@gmail.com>
2021-12-09 12:41:45 -0500

    added api to axios

--- src/components/Api.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const myApi = 'bc5oQEalciMcJmOkmWc5dka0yHixGYW7I5fpJ9aT'
-const nasaUrl = 'https://api.nasa.gov/planetary/apod?api_key='
-
-export {myApi, nasaUrl}
--- src/components/Data.js
+++ src/components/Data.js
@@ -4,20 +4,22 @@ import {myApi, nasaUrl} from './Api';
 
 const Data = () => {
     // Initialize state to hold the image URL
-    const [nasaPic, setNasaPic] = useState("");
+    const [data, setData] = useState({})
+    const [myApi, setApi] = useState('')
+    const setApi = 'bc5oQEalciMcJmOkmWc5dka0yHixGYW7I5fpJ9aT'
     useEffect(() => {
       // This axios GET request will return a single image
       axios
-        .get('https://api.nasa.gov/planetary/apod?api_key=bc5oQEalciMcJmOkmWc5dka0yHixGYW7I5fpJ9aT')
+        .get(`https://api.nasa.gov/planetary/apod?api_key=${myApi}`)
         // Which we then set to state
-        .then(res => setNasaPic(res.data))
+        .then(res => setData(res.data))
         // Always include error handling
         .catch(err => console.log(err));
     }, []);
     return (
       <div className="container">
         <h1>Picture of the day</h1>
-        <img src={nasaPic.url} />
+        <img src={data.url} />
       </div>
     )
 }
